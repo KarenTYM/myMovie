5fa3adaa7a715ff491c31c57b80c4cff08d376dd
Karen Tabuse Yoshimura Makihara <karen_yoshimura@yahoo.com.br>
2021-02-18 16:06:49 -0300

    Cria métodos para fazer requisição da API

--- MyMovie/Network/API.swift
+++ MyMovie/Network/API.swift
@@ -11,5 +11,71 @@ import Alamofire
 private let baseUrl = "https://api.themoviedb.org/3/movie/"
 let posterUrl = "https://image.tmdb.org/t/p/original"
 let backdropUrl = "https://image.tmdb.org/t/p/w300"
-private let apiKey = "04086c212ff4d6f21382347b01ffdc69"
-private let coder = JSONDecoder()
+private let apiKey = "api_key=04086c212ff4d6f21382347b01ffdc69"
+
+class API {
+    class func getPopularMovies(page:Int, onSuccess: @escaping (Results) -> Void) {
+        JSONDecoder().keyDecodingStrategy = .convertFromSnakeCase
+        let urlStr = "\(baseUrl)popular?\(apiKey)&language=pt-BR&page=\(page)"
+        guard let url = URL(string: urlStr) else {fatalError("Unable to get url")}
+        
+        // método da classe Alamofire que pega uma url e traz uma resposta
+        AF.request(url).response { response in
+            switch response.result {
+            case .success(let data):
+                guard let data = data else {fatalError("Unable to parse data from api")}
+                guard let results = try? JSONDecoder().decode(Results.self, from: data) else {fatalError("Unable to parse data in to JSon")}
+                DispatchQueue.main.async {
+                    onSuccess(results)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+  
+    
+    class func getMoviesDetails(movieID: Int, onSucess: @escaping (MovieDetail) -> Void) {
+        JSONDecoder().keyDecodingStrategy = .convertFromSnakeCase
+        let urlStr = "\(baseUrl)\(movieID)?\(apiKey)&language=pt-BR"
+        guard let url = URL(string: urlStr) else {fatalError("Unable to get url")}
+        
+        AF.request(url).response { response in
+            switch response.result {
+            case .success(let data):
+                guard let data = data else { fatalError("Unable fo parse data from api")}
+                guard let details = try? JSONDecoder().decode(MovieDetail.self, from: data) else { fatalError("Unable to parse data in to Json")}
+                print("++++++++++++++++++++++++++ \(details.id)")
+                DispatchQueue.main.async {
+                    onSucess(details)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+
+    class func getMovieFiltered(query:String, page:Int, onSuccess: @escaping (Results) -> Void) {
+        JSONDecoder().keyDecodingStrategy = .convertFromSnakeCase
+        let urlStr = "https://api.themoviedb.org/3/search/movie?\(apiKey)&query=\(query)&language=pt-BR&page=\(page)"
+        guard let url = URL(string: urlStr) else {fatalError("Unable to get url")}
+        
+        // método da classe Alamofire que pega uma url e traz uma resposta
+        AF.request(url).response { response in
+            switch response.result {
+            case .success(let data):
+                guard let data = data else {fatalError("Unable to parse data from api")}
+                guard let results = try? JSONDecoder().decode(Results.self, from: data) else {fatalError("Unable to parse data in to JSon")}
+                print("************************* \(results.results)")
+                DispatchQueue.main.async {
+                    onSuccess(results)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+
---
+++ MyMovie/Network/GenericWorker.swift
@@ -0,0 +1,18 @@
+//
+//  GenericWorker.swift
+//  MyMovie
+//
+//  Created by Fabio Makihara on 17/02/21.
+//
+
+import Foundation
+import UIKit
+
+enum Method:String{
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+
