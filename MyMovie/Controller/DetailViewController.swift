585e8a0d1f9da54d413601f8bbd1df4497fffe6f
Karen Tabuse Yoshimura Makihara <karen_yoshimura@yahoo.com.br>
2021-02-18 16:08:04 -0300

    Faz mostrar filmes populares e chama a tela de detalhes

--- MyMovie/Controller/DetailViewController.swift
+++ MyMovie/Controller/DetailViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var voteCountLabel: UILabel!
+    @IBOutlet weak var voteAverageLabel: UILabel!
+    @IBOutlet weak var budgetLabel: UILabel!
+    @IBOutlet weak var runtimeLabel: UILabel!
+    @IBOutlet weak var releaseDataLabel: UILabel!
+    @IBOutlet weak var overviewTextView: UITextView!
+    
+    var movie:PopularMovie?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.movieImageView.layer.cornerRadius = 5
+        
+        API.getMoviesDetails(movieID: movie?.id ?? 0) { (detail) in
+            self.titleLabel.text = detail.title ?? "No title"
+            self.voteCountLabel.text = "Total de votos: \(String(detail.voteCount ?? 0))"
+            self.voteAverageLabel.text = "Média: \(String(detail.voteAverage ?? 0.0))"
+            self.budgetLabel.text = "R$\(String(describing: detail.budget ?? 0))"
+            self.runtimeLabel.text = "\(String(describing: detail.runtime ?? 0)) minutos"
+            self.releaseDataLabel.text = "Data de lançamento: \(detail.releaseDate ?? "0000-00-00")"
+            self.overviewTextView.text = detail.overview
+            self.movieImageView.downloaded(from: "\(backdropUrl)\(detail.posterPath ?? "")", contentMode: .scaleAspectFill)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- MyMovie/Controller/MoviesViewController.swift
+++ MyMovie/Controller/MoviesViewController.swift
@@ -12,12 +12,17 @@ class MoviesViewController: UIViewController {
     @IBOutlet weak var searchrBar: UISearchBar!
     @IBOutlet weak var movieTableView: UITableView!
     
+    private var movieArray:[PopularMovie]?
+    private var page:Int = 1
+    private var totalPages:Int = 0
+    
     
     // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.configView()
+        self.fetchMovies()
     }
     
     
@@ -25,8 +30,32 @@ class MoviesViewController: UIViewController {
     private func configView() {
         self.movieTableView.delegate = self
         self.movieTableView.dataSource = self
+        self.searchrBar.delegate = self
         
         self.movieTableView.register(UINib(nibName: "MovieCell", bundle: nil), forCellReuseIdentifier: "MovieCell")
+    }
+    
+    
+    // MARK: - fetchMovies
+    private func fetchMovies(page:Int = 1) {
+        API.getPopularMovies(page:page) { data in
+            self.totalPages = data.totalPages ?? 1
+            self.movieArray = data.results
+            self.movieTableView.reloadData()
+        }
+    }
+    
+    // MARK: - loadMoreData
+    private func loadMoreData() {
+        if self.page < self.totalPages {
+            self.page += 1
+            OperationQueue.main.addOperation {
+                API.getPopularMovies(page: self.page) { (data) in
+                    self.movieArray? += data.results
+                    self.movieTableView.reloadData()
+                }
+            }
+        }
     }
 }
 
@@ -34,14 +63,46 @@ class MoviesViewController: UIViewController {
 // MARK: - extension TableViewDelegate, TableViewDataSource
 extension MoviesViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return self.movieArray?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:MovieCell? = self.movieTableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? MovieCell
+        guard let _movieArray = self.movieArray?[indexPath.row] else {fatalError()}
+        cell?.setupCell(movie: _movieArray)
         return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let count = self.movieArray?.count else {fatalError()}
+        if indexPath.row == count - 1 {
+            self.loadMoreData()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "DetailViewController", sender: self.movieArray?[indexPath.row])
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let movie:PopularMovie? = sender as? PopularMovie
+        let vc = segue.destination as? DetailViewController
+        vc?.movie = movie
     }
 }
 
 
-
+// MARK: - extension UISearchBarDelegate
+extension MoviesViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchrBar.text else {return}
+        let query = text.replacingOccurrences(of: " ", with: "%20")
+        API.getMovieFiltered(query: query, page: 1) { (data) in
+            self.totalPages = data.totalPages ?? 1
+            self.movieArray = data.results
+            self.movieTableView.reloadData()
+        }
+        searchBar.resignFirstResponder()
+        searchBar.text = ""
+    }
+}
